c412311bc124b76635dadf68e94600e82b7ff933
BushraTariq1624 <tbushra211@gamil.com>
2024-11-11 20:48:03 +0500

    Firebase

---
+++ Assignment 1/firebase.mjs
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth,createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyCxwQBwJTzn5sQi98f-jW4bOHvzdnoXy1I",
+    authDomain: "bushraproject-5b223.firebaseapp.com",
+    projectId: "bushraproject-5b223",
+    storageBucket: "bushraproject-5b223.firebasestorage.app",
+    messagingSenderId: "213120765889",
+    appId: "1:213120765889:web:4a4a9c58a655801db02940"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export{auth, createUserWithEmailAndPassword}
